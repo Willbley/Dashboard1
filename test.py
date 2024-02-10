02d8035b2997d665ac9615e1f769393d65026c83
willbley <williancaputobley@outlook.com.br>
2024-02-10 04:15:47 -0300

    Atualização no arquivo test.py

--- test.py
+++ test.py
@@ -1,17 +1,20 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
-import plotly.express as px
 
+# Criando um DataFrame de exemplo
 df = pd.DataFrame(
     np.random.rand(10, 4),
     columns=['Preço', 'Taxa de ocupação', 'Taxa de inadimplência', 'Pessoas por casa']
 )
 
+# Título principal
 st.title('Dashboard Example')
-st.header('Gráfico de Linha')
-line_chart = px.line(df, title='Tendência ao longo do tempo')  # Fixing the typo in 'dfm' and 'title'
-st.plotly_chart(line_chart, use_container_width=True)
 
+# Gráfico de Linha usando st.line_chart do próprio Streamlit
+st.header('Gráfico de Linha')
+st.line_chart(df)
+
+# Dados em Tabela
 st.header('Dados em Tabela')
 st.table(df)
